9e6aa2756bbb811c5390cb7865958eb9d8488872
Mann9939 <72181425+Mann9939@users.noreply.github.com>
2023-08-27 08:00:59 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/Components/header.css';
+import '../styles/Components/about.css';
+import '../styles/Components/featured.css';
+import '../styles/utils.css';
+import '../styles/Components/contact.css';
